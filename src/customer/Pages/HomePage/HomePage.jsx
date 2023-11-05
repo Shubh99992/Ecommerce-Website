e67e2dfab90bc772803d2cc068a9eea316297fbe
Shubham <coderblade7@gmail.com>
2023-11-05 15:59:55 +0530

    Adding homepage

---
+++ src/customer/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCrousel from '../../components/HomeCarousel/MainCarousel'
+
+function HomePage() {
+  return (
+    <div>
+        <MainCrousel/>
+        <div>
+            other section
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
