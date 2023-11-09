fda334ee4cf697e7968f4d7208fa0d0d8ab9e3be
Shubham <coderblade7@gmail.com>
2023-11-09 12:12:47 +0530

    Adding Navigation

--- src/customer/components/Navigation/Navigation.jsx
+++ src/customer/components/Navigation/Navigation.jsx
@@ -2,129 +2,130 @@
 import { Fragment, useState } from 'react'
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { navigation } from './navigationData' 
 
-const navigation = {
-  categories: [
-    {
-      id: 'women',
-      name: 'Women',
-      featured: [
-        {
-          name: 'New Arrivals',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-          imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
-        },
-        {
-          name: 'Basic Tees',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-          imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-        },
-      ],
-      sections: [
-        {
-          id: 'clothing',
-          name: 'Clothing',
-          items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Dresses', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Denim', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
-          ],
-        },
-        {
-          id: 'accessories',
-          name: 'Accessories',
-          items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Significant Other', href: '#' },
-          ],
-        },
-      ],
-    },
-    {
-      id: 'men',
-      name: 'Men',
-      featured: [
-        {
-          name: 'New Arrivals',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
-          imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
-        },
-        {
-          name: 'Artwork Tees',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
-          imageAlt:
-            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
-        },
-      ],
-      sections: [
-        {
-          id: 'clothing',
-          name: 'Clothing',
-          items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
-          ],
-        },
-        {
-          id: 'accessories',
-          name: 'Accessories',
-          items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
-          ],
-        },
-      ],
-    },
-  ],
-  pages: [
-    { name: 'Company', href: '#' },
-    { name: 'Stores', href: '#' },
-  ],
-}
+// const navigation = {
+//   categories: [
+//     {
+//       id: 'women',
+//       name: 'Women',
+//       featured: [
+//         {
+//           name: 'New Arrivals',
+//           href: '#',
+//           imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+//           imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+//         },
+//         {
+//           name: 'Basic Tees',
+//           href: '#',
+//           imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+//           imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+//         },
+//       ],
+//       sections: [
+//         {
+//           id: 'clothing',
+//           name: 'Clothing',
+//           items: [
+//             { name: 'Tops', href: '#' },
+//             { name: 'Dresses', href: '#' },
+//             { name: 'Pants', href: '#' },
+//             { name: 'Denim', href: '#' },
+//             { name: 'Sweaters', href: '#' },
+//             { name: 'T-Shirts', href: '#' },
+//             { name: 'Jackets', href: '#' },
+//             { name: 'Activewear', href: '#' },
+//             { name: 'Browse All', href: '#' },
+//           ],
+//         },
+//         {
+//           id: 'accessories',
+//           name: 'Accessories',
+//           items: [
+//             { name: 'Watches', href: '#' },
+//             { name: 'Wallets', href: '#' },
+//             { name: 'Bags', href: '#' },
+//             { name: 'Sunglasses', href: '#' },
+//             { name: 'Hats', href: '#' },
+//             { name: 'Belts', href: '#' },
+//           ],
+//         },
+//         {
+//           id: 'brands',
+//           name: 'Brands',
+//           items: [
+//             { name: 'Full Nelson', href: '#' },
+//             { name: 'My Way', href: '#' },
+//             { name: 'Re-Arranged', href: '#' },
+//             { name: 'Counterfeit', href: '#' },
+//             { name: 'Significant Other', href: '#' },
+//           ],
+//         },
+//       ],
+//     },
+//     {
+//       id: 'men',
+//       name: 'Men',
+//       featured: [
+//         {
+//           name: 'New Arrivals',
+//           href: '#',
+//           imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
+//           imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
+//         },
+//         {
+//           name: 'Artwork Tees',
+//           href: '#',
+//           imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
+//           imageAlt:
+//             'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+//         },
+//       ],
+//       sections: [
+//         {
+//           id: 'clothing',
+//           name: 'Clothing',
+//           items: [
+//             { name: 'Tops', href: '#' },
+//             { name: 'Pants', href: '#' },
+//             { name: 'Sweaters', href: '#' },
+//             { name: 'T-Shirts', href: '#' },
+//             { name: 'Jackets', href: '#' },
+//             { name: 'Activewear', href: '#' },
+//             { name: 'Browse All', href: '#' },
+//           ],
+//         },
+//         {
+//           id: 'accessories',
+//           name: 'Accessories',
+//           items: [
+//             { name: 'Watches', href: '#' },
+//             { name: 'Wallets', href: '#' },
+//             { name: 'Bags', href: '#' },
+//             { name: 'Sunglasses', href: '#' },
+//             { name: 'Hats', href: '#' },
+//             { name: 'Belts', href: '#' },
+//           ],
+//         },
+//         {
+//           id: 'brands',
+//           name: 'Brands',
+//           items: [
+//             { name: 'Re-Arranged', href: '#' },
+//             { name: 'Counterfeit', href: '#' },
+//             { name: 'Full Nelson', href: '#' },
+//             { name: 'My Way', href: '#' },
+//           ],
+//         },
+//       ],
+//     },
+//   ],
+//   pages: [
+//     { name: 'Company', href: '#' },
+//     { name: 'Stores', href: '#' },
+//   ],
+// }
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -307,11 +308,11 @@ export default function Navigation() {
               </div>
 
               {/* Flyout menus */}
-              <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
+              <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch z-10">
                 <div className="flex h-full space-x-8">
                   {navigation.categories.map((category) => (
                     <Popover key={category.name} className="flex">
-                      {({ open }) => (
+                      {({ open, close }) => (
                         <>
                           <div className="relative flex">
                             <Popover.Button
