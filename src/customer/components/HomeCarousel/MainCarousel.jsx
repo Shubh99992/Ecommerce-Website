3969708162f55f9c8e7f8af651d1b06842a16ae2
Shubham <coderblade7@gmail.com>
2023-11-09 12:17:21 +0530

    Adding Main Carousel

--- src/customer/components/HomeCarousel/MainCarousel.jsx
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import {MainCrouselData} from './MainCrouselData';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import React from "react";
+import { MainCrouselData } from "./MainCrouselData";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
-const MainCarousel = ()=> {
-
-    const items = MainCrouselData.map((item)=> <img className= 'cursor-pointer'role='presentation' src={item.image} alt=""/>)
+const MainCarousel = () => {
+  const items = MainCrouselData.map((item) => (
+    <img
+      className="cursor-pointer"
+      role="presentation"
+      src={item.image}
+      alt=""
+    />
+  ));
 
   return (
     <AliceCarousel
-    items={items}
-    disableButtonsControls
-    autoPlay
-    autoPlayInterval={1000}
-    infinite
-/>
-  )
-}
+      items={items}
+      disableButtonsControls
+      autoPlay
+      autoPlayInterval={1000}
+      infinite
+    />
+  );
+};
 
-export default MainCarousel
+export default MainCarousel;
