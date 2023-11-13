789367ca5a71dd98dcedcdd143ff6037b0ecf251
Shubham <coderblade7@gmail.com>
2023-11-13 20:33:33 +0530

    Footer code Updated {Responsiveness done }

--- src/customer/components/Footer/Footer.jsx
+++ src/customer/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { Button, Grid, Link } from "@mui/material";
 import { Typography } from "@mui/material";
 import React from "react";
+
 function Footer() {
   return (
     <div>
@@ -9,137 +10,120 @@ function Footer() {
         container
         sx={{ bgcolor: "black", color: "white", py: 3 }}
       >
-        <Grid item sx={12} sm={6} md={3}>
-          <Typography className="pb-5" variant="h6">
-            {""}
+        <Grid item xs={12} sm={6} md={3} lg={3}>
+          <Typography variant="h6" sx={{ fontWeight: 'bold', pb: 2 }}>
             Company
           </Typography>
 
           <div>
-            <Button className="pb-5" variant="h6">
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               About
             </Button>
           </div>
 
           <div>
-            <Button className="pb-5" variant="h6">
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               Blogs
             </Button>
           </div>
 
           <div>
-            <Button className="pb-5" variant="h6">
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               Jobs
             </Button>
           </div>
 
           <div>
-            <Button className="pb-5" variant="h6">
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               Partners
             </Button>
           </div>
         </Grid>
 
-        <Grid item sx={12} sm={6} md={3}>
-          <Typography className="pb-5" variant="h6">
-            {""}
-            Sulution
+        <Grid item xs={12} sm={6} md={3} lg={3}>
+          <Typography variant="h6" sx={{ fontWeight: 'bold', pb: 2 }}>
+            Solution
           </Typography>
           <div>
-            <Button className="pb-5" variant="h6" gutterButton>
-              {""}
-              Markiting
+            <Button variant="body2" sx={{ pb: 2 }}>
+              Marketing
             </Button>
           </div>
 
           <div>
-            <Button className="pb-5" variant="h6" gutterButton>
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               Commerce
             </Button>
           </div>
 
           <div>
-            <Button className="pb-5" variant="h6" gutterButton>
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               Insights
             </Button>
           </div>
 
           <div>
-            <Button className="pb-5" variant="h6" gutterButton>
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               Support
             </Button>
           </div>
         </Grid>
 
-        <Grid item sx={12} sm={6} md={3}>
-          <Typography className="pb-5" variant="h6">
-            {""}
-            Documentaion
+        <Grid item xs={12} sm={6} md={3} lg={3}>
+          <Typography variant="h6" sx={{ fontWeight: 'bold', pb: 2 }}>
+            Documentation
           </Typography>
           <div>
-            <Button className="pb-5" variant="h6" gutterButton>
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               API Integration
             </Button>
           </div>
 
           <div>
-            <Button className="pb-5" variant="h6" gutterButton>
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               API Status
             </Button>
           </div>
         </Grid>
 
-        <Grid item sx={12} sm={6} md={3}>
-          <Typography className="pb-5" variant="h6">
-            {""}
+        <Grid item xs={12} sm={6} md={3} lg={3}>
+          <Typography variant="h6" sx={{ fontWeight: 'bold', pb: 2 }}>
             Legal
           </Typography>
           <div>
-            <Button className="pb-5" variant="h6" gutterButton>
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               Claim
             </Button>
           </div>
 
           <div>
-            <Button className="pb-5" variant="h6" gutterButton>
-              {""}
+            <Button variant="body2" sx={{ pb: 2 }}>
               Privacy
             </Button>
           </div>
 
           <div>
-            <Button className="pb-5" variant="h6" gutterButton>
-              {""}
-              Term & Condition
+            <Button variant="body2" sx={{ pb: 2 }}>
+              Terms & Conditions
             </Button>
           </div>
         </Grid>
 
-        <Grid className="pt-20" item xs={12}>
+        <Grid className="pt-10" item xs={12}>
           <Typography variant="body2" component="p" align="center">
-            &copy; 2023 My Company. All rights reserved.{""}
+            &copy; 2023 Shubham. All rights reserved.
           </Typography>
 
           <Typography variant="body2" component="p" align="center">
-            Made with me {""}
+            Made with ❤️ by Shubham
           </Typography>
 
           <Typography variant="body2" component="p" align="center">
-            Icons made by {""}
+            Icons made by{" "}
             <Link href="#" color="inherit" underline="always">
               Freepik
-            </Link>
-            {""} from {""}
+            </Link>{" "}
+            from{" "}
             <Link href="#" color="inherit" underline="always">
               Freepik
             </Link>
