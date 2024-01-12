4206579372fd6d9a82e34a5e951d01d6546db216
Shubham <coderblade7@gmail.com>
2024-01-12 22:45:56 +0530

    SpeedInsights fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import CustomerRouters from './Routers/CustomerRouters.jsx';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <div className="">
+      <div><SpeedInsights /></div>
       <Routes>
         <Route path='/*' element={<CustomerRouters/>}></Route>
       </Routes>
